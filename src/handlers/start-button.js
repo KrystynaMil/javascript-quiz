9e0d47c0e8eda4f82b7aee3a6895d39c83733e6a
KrystynaMil <kristina-manager@hotmail.com>
2020-12-22 14:26:17 +0100

    start-button handler and listener

---
+++ src/handlers/start-button.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+
+/*show quiz*/
+
+  function startQuiz() {
+
+    document.getElementById('show').style.display = "block";
+    document.getElementById('starter').style.display = 'none';
+    
+    
+    getNewQuestion();
+}
+
+
+
+
+/* handlers define user interactions
+
+  they read user input
+    from events or from the DOM
+
+  they process user data using program logic functions
+
+  they communicate results to the user
+
+  they log any important information for developers
+
+*/
